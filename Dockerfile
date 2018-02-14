5fea4b3facc62356121ae29baac5f1512925a75a
Denis T <tribushkov@gmail.com>
2018-02-14 15:13:46 +0300

    added jdk and maven bootstrap

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM nvidia/cuda-ppc64le:8.0-cudnn6-devel-ubuntu16.04
 
 COPY ppc64le/* /ppc64le/
 RUN /ppc64le/powerai.sh
+RUN /ppc64le/jdk_maven.sh
---
+++ ppc64le/jdk_maven.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e -x
+export MAVEN_REPO=apache-maven-3.5.2-bin.tar.gz
+
+# install OpenJDK http://openjdk.java.net/install/
+apt-get update && apt-get install -y \
+    openjdk-8-jdk \
+    openjfx \
+    apt-get clean
+
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-ppc64el' >> ~/.bashrc
+
+# download Apache Maven https://maven.apache.org/install.html
+cd /opt && \
+    wget http://www-eu.apache.org/dist/maven/maven-3/3.5.2/binaries/${MAVEN_REPO}  && \
+    tar -xvzf ${MAVEN_REPO} && \
+    mv apache-maven-3.5.2 maven
+
+echo 'export PATH=$PATH:~/opt/maven/bin' >> ~/.bashrc 
